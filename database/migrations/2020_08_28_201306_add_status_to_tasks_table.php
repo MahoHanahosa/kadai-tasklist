863a77f3c6cb126f6b64b68037dd2badee67b580
maho.hanahosa <hanamaho.bump@icloud.com>
2020-08-29 12:06:22 +0000

    マイグレーション変更

--- database/migrations/2020_08_28_201306_add_status_to_tasks_table.php
+++ database/migrations/2020_08_28_201306_add_status_to_tasks_table.php
@@ -14,8 +14,8 @@ class AddStatusToTasksTable extends Migration
     public function up()
     {
         Schema::table('tasks', function (Blueprint $table) {
-            $table->string('status');
-            $table->string('status', 10)->change();
+            
+            $table->string('status', 10);
         });
     }
 
